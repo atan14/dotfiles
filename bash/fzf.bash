fc7b88b775b02dcbe2cac33f438543a753f5e978
atan14 <atan14@mit.edu>
2022-05-25 14:32:35 -0400

    reverted change of path from fzf to .fzf

--- bash/fzf.bash
+++ bash/fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/atan14/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/atan14/fzf/bin"
+if [[ ! "$PATH" == */home/atan14/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/atan14/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/atan14/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/home/atan14/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/atan14/fzf/shell/key-bindings.bash"
+source "/home/atan14/.fzf/shell/key-bindings.bash"
